d0ff53cd5ffc4f47c7bb077407406002b54921be
RonaldZboj <ronaldzboj@gmail.com>
2017-04-30 21:27:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/integratorplus/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.integratorplus;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToSo(View view) {
+        goToUrl("http://integratorplus.pl/login/?next=/dashboard/");
+    }
+
+
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+}
